bed5676baf1b7bab98a1889ba322a8350625d122
Lin Tong <lin_tong@hotmail.com>
2021-12-08 14:25:17 -0500

    Updates

---
+++ precache-manifest.45f1cf6e40061cea8539f20e3b06a9a0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea2ed93d4095513d8da4ba5e6abe7eb5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "76f5da5426bd5a1b776d",
+    "url": "/robofriends/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "e88a74a7929e23d2ae7d",
+    "url": "/robofriends/static/css/main.0462f600.chunk.css"
+  },
+  {
+    "revision": "76f5da5426bd5a1b776d",
+    "url": "/robofriends/static/js/2.1d1af241.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/robofriends/static/js/2.1d1af241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627485cada7abdc42d0d",
+    "url": "/robofriends/static/js/3.c6b94e83.chunk.js"
+  },
+  {
+    "revision": "e88a74a7929e23d2ae7d",
+    "url": "/robofriends/static/js/main.b464314e.chunk.js"
+  },
+  {
+    "revision": "8e7d61bbca9a5cac374d",
+    "url": "/robofriends/static/js/runtime-main.4866d8e2.js"
+  }
+]);
